b85b0909ebd7600b9faf245c41a6cad6dae0d0d4
Matyáš Strelec <matyasstrelec@gmail.com>
2023-04-10 17:57:42 +0200

    feat: add expression parser&solver + UDP server needs refactor

--- ipkcpd.cpp
+++ ipkcpd.cpp
@@ -11,9 +11,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <list>
 #include <iostream>
 #include <string>
+#include <stack>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 // No need to write std:: for everything
 using namespace std;
@@ -56,6 +66,80 @@ args_t parse_args(int argc, char** argv) {
     return args;
 }
 
+//    (+ 111 (* 432 3))
+// https://www.geeksforgeeks.org/split-a-sentence-into-words-in-cpp/
+list<string> parse_expression(string expression) {
+    list<string> tokens;
+    string new_expression;
+
+    for(int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '(' || expression[i] == ')') {
+            new_expression += " ";
+            new_expression += expression[i];
+            new_expression += " ";
+        } else {
+            new_expression += expression[i];
+        }
+    }
+
+    istringstream ss(new_expression);
+    string token;
+
+    while (ss >> token)
+    {
+        tokens.insert(tokens.end(), token);
+    }
+
+    return tokens;
+}
+
+int solve_expression(string expression)
+{
+    list<string> tokens = parse_expression(expression);
+
+    stack<string> stack;
+
+    for (string token : tokens)
+    {
+        if(token != ")")
+        {
+            stack.push(token);
+        }
+        else
+        {
+            int result = 0;
+            int operand2 = stoi(stack.top());
+            stack.pop();
+            int operand1 = stoi(stack.top());
+            stack.pop();
+            string operation = stack.top();
+            stack.pop();
+            stack.pop();
+
+            if (operation == "+")
+            {
+                result = operand1 + operand2;
+            }
+            else if (operation == "-")
+            {
+                result = operand1 - operand2;
+            }
+            else if (operation == "*")
+            {
+                result = operand1 * operand2;
+            }
+            else if (operation == "/")
+            {
+                result = operand1 / operand2;
+            }
+
+            stack.push(to_string(result));
+        }
+    }
+
+    return stoi(stack.top());
+}
+
 /**
  * @brief TCP server
  *
@@ -68,6 +152,8 @@ int tcp_server(string host, string port) {
     return 0;
 }
 
+#define BUFSIZE 512
+
 /**
  * @brief UDP server
  *
@@ -76,7 +162,72 @@ int tcp_server(string host, string port) {
  * @return int
  */
 int udp_server(string host, string port) {
-    cout << "UDP server" << endl;
+    char buf[BUFSIZE];
+	int server_socket, port_number, bytestx, bytesrx;
+    socklen_t clientlen;
+    struct sockaddr_in client_address, server_address;
+    int optval;
+    const char * hostaddrp;
+    struct hostent *hostp;
+    port_number = atoi(port.c_str());
+
+    /* Vytvoreni soketu */
+	if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) <= 0)
+	{
+		perror("ERROR: socket");
+		exit(EXIT_FAILURE);
+	}
+    /* potlaceni defaultniho chovani rezervace portu ukonceni aplikace */ 
+    optval = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+    
+
+    /* adresa serveru, potrebuje pro prirazeni pozadovaneho portu */
+    bzero((char *) &server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons((unsigned short)port_number);
+
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    // if (  0) 
+    // {
+    //     perror("ERROR: binding");
+    //     exit(EXIT_FAILURE);
+    // }
+    
+    while(1) 
+    {   
+        printf("INFO: Ready.\n");
+    	/* prijeti odpovedi a jeji vypsani */
+        clientlen = sizeof(client_address);
+        bytesrx = recvfrom(server_socket, buf, BUFSIZE, 0, (struct sockaddr *) &client_address, &clientlen);
+        if (bytesrx < 0) 
+            perror("ERROR: recvfrom:");
+    
+        hostp = gethostbyaddr((const char *)&client_address.sin_addr.s_addr, 
+			  sizeof(client_address.sin_addr.s_addr), AF_INET);
+              
+        hostaddrp = inet_ntoa(client_address.sin_addr);
+    
+        char opcode = buf[0];
+        char len = buf[1];
+        char * data = buf+2;
+
+        int res = solve_expression(data);
+        
+        bzero(buf, BUFSIZE);
+
+        buf[0] = '\1';
+        buf[1] = '\0';
+        buf[2] = (char)to_string(res).length();
+        strcpy(buf+3, to_string(res).c_str());
+
+        /* odeslani zpravy zpet klientovi  */        
+        bytestx = sendto(server_socket, buf, buf[2]+3, 0, (struct sockaddr *) &client_address, clientlen);
+        if (bytestx < 0) 
+            perror("ERROR: sendto:");
+    }
+
     return 0;
 }
 
@@ -88,8 +239,14 @@ int udp_server(string host, string port) {
  * @return Exit code
  */
 int main(int argc, char** argv) {
-    // Load arguments to global variable
-    args = parse_args(argc, argv);
+    // list<string> tokens = parse_expression("(/ (+ 7 3) 2)");
+    // int result = solve_expression(tokens);
+    // // Load arguments to global variable
+    // // args = parse_args(argc, argv);
+    // args_t args;
+    args.host = "localhost";
+    args.port = "2023";
+    args.mode = "udp";
 
     // Check if host is correct
     if (args.host.empty()) {
