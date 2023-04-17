e61bf99a48fc26445197ecc8e7fcfe87808eed45
Matyáš Strelec <matyasstrelec@gmail.com>
2023-04-17 17:50:11 +0200

    test: update script & add test

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# echo time
+echo "Running ipkcpd tests at $(date)"
+printf "\n"
+
 # stop the server
 killall ipkcpd
 
 # go through all files in tests directory
 # and run them as input
 
+echo "-- TCP TESTS --"
 # start the server in the background
 ./ipkcpd -m tcp -h localhost -p 3333 &
 # wait for the server to start
@@ -14,17 +19,20 @@ sleep 1
 # for all files
 for file in tests/in/tcp/*;
 do
-    echo "Running test $file"
+    # printf running test with tab in front
+    printf "\tRunning test %s\n" "$file"
     # run the test, get the output
     output=$(./ipkcpc -m tcp -p 3333 -h localhost < "$file")
     # compare output with expected output
     diff <(echo "$output") "tests/out/tcp/$(basename "$file")"
 done
+printf "\n"
 
 # stop the server
 killall ipkcpd
 
 # start the server in the background
+echo "-- UDP TESTS --"
 ./ipkcpd -m udp -h localhost -p 4444 &
 # wait for the server to start
 sleep 1
@@ -32,7 +40,7 @@ sleep 1
 # for all files
 for file in tests/in/udp/*;
 do
-    echo "Running test $file"
+    printf "\tRunning test %s\n" "$file"
     # run the test, get the output
     output=$(./ipkcpc -m udp -p 4444 -h localhost < "$file")
     # compare output with expected output
